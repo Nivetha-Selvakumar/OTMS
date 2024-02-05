f7e6014a9097102921fb951fa57209d0b0310a02
Nivetha-Selvakumar <nivethaselvakumar23@gmail.com>
2024-02-05 16:16:28 +0530

    fifth commit

---
+++ src/main/java/com/onlinetaskmanagementsystem/otms/Response/SignUpResponse.java
@@ -0,0 +1,23 @@
+package com.onlinetaskmanagementsystem.otms.Response;
+
+public class SignUpResponse {
+
+    String message;
+    String userId;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
