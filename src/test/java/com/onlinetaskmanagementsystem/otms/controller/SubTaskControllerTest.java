5debb8fca9ddca30af75441f51d64af92ef800b3
Nivetha-Selvakumar <nivethaselvakumar23@gmail.com>
2024-03-12 18:15:19 +0530

    Fourteen commit

--- src/test/java/com/onlinetaskmanagementsystem/otms/controller/SubTaskControllerTest.java
+++ src/test/java/com/onlinetaskmanagementsystem/otms/controller/SubTaskControllerTest.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
